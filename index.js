eb4f2369c3694a69a9a7cbd6bd3f5886a2b6ea1f
Dyasanti V <506thinfantry@gmail.com>
2018-05-05 18:37:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,14 +12,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-<<<<<<< HEAD
   var newObj = Object.assign({}, object)
   delete newObj[key]
   return newObj
-=======
   delete Object.assign({}, object)[key];
   return object 
->>>>>>> 34466eb86b30c7b33b76f3a8daa98fdf87aaf5e8
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
